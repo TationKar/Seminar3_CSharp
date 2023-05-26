3aef9f76244060429baa33cca22eda702a538ac5
Timur Karimov <tationkar@gmail.com>
2023-05-26 20:23:57 +0300

    Task 1

--- Sem3_1/Program.cs
+++ Sem3_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// **Задача 18:**
+// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+Console.Clear();
+Console.Write("введите число от 1 до 4: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num == 1)
+    Console.WriteLine("X > 0, Y > 0");
+else if (num == 2)
+    Console.WriteLine("X < 0, Y > 0");
+else if (num == 3)
+    Console.WriteLine("X < 0, Y < 0");
+else if (num == 4)
+    Console.WriteLine("X > 0, Y < 0");
+
