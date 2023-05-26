a331997a5508a5c894bd5f95b8b3db4cb604cdcc
Timur Karimov <tationkar@gmail.com>
2023-05-26 21:09:50 +0300

    Task 3

---
+++ Sem3_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+Console.Clear();
+Console.Write("Введите целое число: ");
+int num = int.Parse(Console.ReadLine());
+int count = 1;
+
+if (num == 0 )
+    Console.Write("0");
+
+while (count <= num)
+{
+    Console.Write(Math.Pow(count, 2));
+    Console.Write(" ");
+    count++;
+}
+Console.WriteLine();
